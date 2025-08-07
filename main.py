e901522592c25a79d3f91ebcc9a0682cb30aff22
Jmserge29 <Jmserge29@gmail.com>
2025-08-07 17:57:56 -0500

    Inicializar proyecto ToDoApp

---
+++ main.py
@@ -0,0 +1,9 @@
+from task_model import 
+
+def main():
+    task1 = TaskModel("Estudiar para el examen")
+    print(f"Tarea creada: {task1.get_task_name()}")
+
+if __name__ == "__main__":
+    main()
+
